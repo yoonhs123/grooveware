bc98fcbd09d05c559cfafd77012f0593fd2c181b
WONJAE <WONJAE@DESKTOP-GT7QAFM>
2023-07-06 22:44:38 +0900

    자료실 상위 폴더 업데이트

--- grooveware/src/main/java/com/sp/grooveware/archive/ArchiveController.java
+++ grooveware/src/main/java/com/sp/grooveware/archive/ArchiveController.java
@@ -44,79 +44,11 @@ public class ArchiveController {
 	 private FileManager fileManager;
 	 
 	
-	
-	@RequestMapping(value="list")
-	public String list(@RequestParam(value="page", defaultValue="1") int current_page,
-			@RequestParam(defaultValue="all") String condition,
-			@RequestParam(defaultValue="") String keyword,
-			HttpServletRequest req,
-			Model model) throws Exception {
-		
-		int size = 10;
-		int total_page = 0;
-		int dataCount = 0;
-		
-		if(req.getMethod().equalsIgnoreCase("GET")) {
-			keyword = URLDecoder.decode(keyword, "utf-8");
-		}
-		
-		Map<String, Object> map = new HashMap<String, Object>();
-		map.put("condition", condition);
-		map.put("keyword", keyword);
-		
-		dataCount = service.dataCount(map);
-		
-		if (dataCount != 0) {
-			total_page = myUtil.pageCount(dataCount, size);
-		}
-		// 다른 사람이 자료를 삭제하여 전체 페이지수가 변화 된 경우
-		if (total_page < current_page) {
-			current_page = total_page;
-		}
-		System.out.println(dataCount);
-		System.out.println(total_page);
-		System.out.println(current_page);
-		// 리스트에 출력할 데이터를 가져오기
-		int offset = (current_page - 1) * size;
-		if(offset < 0) offset = 0;
-		
-		map.put("offset", offset);
-		map.put("size", size);
-		
-		List<Archive> list = service.listArchive(map);
-
-		String cp = req.getContextPath();
-		String query = "";
-		String listUrl = cp + "/archive/list";
-		
-		String paging = myUtil.paging(current_page, total_page, listUrl);
-		
-		if (keyword.length() != 0) {
-			query += "&condition=" + condition + "&keyword=" + URLEncoder.encode(keyword, "UTF-8");
-		}
-		if (query.length() != 0) {
-			listUrl = cp + "/admin/noticeManage/list?" + query;
-			}
-		
-		model.addAttribute("list", list);
-		model.addAttribute("dataCount", dataCount);
-		model.addAttribute("size", size);
-		model.addAttribute("page", current_page);
-		model.addAttribute("total_page", total_page);
-		model.addAttribute("paging", paging);
-
-		model.addAttribute("condition", condition);
-		model.addAttribute("keyword", keyword);
-		
-		return ".archive.list";
-	}
-	
 	@RequestMapping(value = "upload", method = RequestMethod.POST)
 	public String writeSubmit(Archive dto,
 			HttpSession session) throws Exception {
 		SessionInfo info = (SessionInfo)session.getAttribute("member");
 		dto.setEmp_no(info.getEmp_no());
-		System.out.println("emp_no: " + dto.getEmp_no());
 		try {
 			
 			String root = session.getServletContext().getRealPath("/");
@@ -255,21 +187,9 @@ public class ArchiveController {
 		
 	}
 	
-	/*
-	 * @RequestMapping(value="list2") public String list(Model model) throws
-	 * Exception { // 최상위 폴더 정보 조회
-	 * 
-	 * Archive root_folder = service.getTopLevelFolders();
-	 * 
-	 * 
-	 * // 모델에 데이터를 담아 JSP로 전달합니다. model.addAttribute("folder", root_folder); //
-	 * list2.jsp로 이동합니다. return ".archive.list2";
-	 * 
-	 * }
-	 */
-	
 	@RequestMapping(value="archive/{folder_no}")
 	public String folder(@PathVariable long folder_no,
+			@RequestParam(defaultValue = "1") long preFolder_no,
 			Model model,
 			HttpSession session
 			) throws Exception {
@@ -284,8 +204,6 @@ public class ArchiveController {
 		
 		root_folderNum = service.getTopLevelFolderNum(folder_no);
 		
-		System.out.println(emp_no);
-		System.out.println(root_folderNum);
 		
 		if(root_folderNum == 1) {
 			sub_folders = service.getSubFolders(folder_no);
@@ -308,6 +226,7 @@ public class ArchiveController {
 	    model.addAttribute("sub_files", sub_files);
 	    model.addAttribute("msg", msg);
 	    model.addAttribute("folder_no", folder_no);
+	    model.addAttribute("preFolder_no", preFolder_no);
 	    // list2.jsp로 이동합니다.
 	    return ".archive.list2";
 		
